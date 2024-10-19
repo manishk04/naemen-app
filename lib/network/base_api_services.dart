4ab80169ff407d438a84f4a11e6d6730ca434d67
Manish <roshan.gupta@vebsigns.com>
2024-10-19 19:43:05 +0530

    Roshan changes

--- lib/network/base_api_services.dart
+++
@@ -1,7 +0,0 @@
-abstract class BaseApiServices {
-  Future<dynamic> getGetApiResponse(String url,
-      Map<String, dynamic>? queryParams, Map<String, String>? headers);
-  Future<dynamic> getPostApiResponse(
-      String url, dynamic data, Map<String, String>? headers,
-      {bool isFormData});
-}
--- lib/utils/app_constants.dart
+++
@@ -1,7 +0,0 @@
-class AppConstants {
-  static const String username = "App-Developer";
-  static const String password = "dG9rZW5AMjAwMw==";
-
-  static const String googleMapAPIKey =
-      "AIzaSyDqPCsU6VM4g8Y6G7VObB2hfa_GLHjTtHQ";
-}
